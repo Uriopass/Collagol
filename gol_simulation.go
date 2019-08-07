04cb6be5a1fb11678afd3355052f287366b6e03e
Paris Douady <paris.douady@adevinta.com>
2019-08-07 11:33:42 +0200

    Add pubsub mechanism for sync

--- gol_simulation.go
+++ gol_simulation.go
@@ -1,54 +1,96 @@
 package main
 
+import "time"
+
+type subType struct {
+	id int
+	c  chan [][]int
+}
+
 type golState struct {
 	grid          [][]int
-	tmpGrid       [][]int
 	width, height int
+	activateCell  chan point
+	updates       map[int]chan [][]int
+	unsub         chan int
+	sub           chan subType
 }
 
 func newGolState(width, height int) *golState {
 	gs := golState{
-		grid:    make([][]int, height),
-		tmpGrid: make([][]int, height),
-		width:   width,
-		height:  height,
+		grid:   make([][]int, height),
+		width:  width,
+		height: height,
 	}
 	for i := 0; i < height; i++ {
 		gs.grid[i] = make([]int, width)
-		gs.tmpGrid[i] = make([]int, width)
 	}
 	return &gs
 }
 
-func (gs *golState) nextTimeStep() {
-	newGrid := make([][]int, gs.height)
+func (gs *golState) nextTimeStep() [][]int {
+	tmpGrid := make([][]int, gs.height)
 	for i := 0; i < gs.height; i++ {
+		tmpGrid[i] = make([]int, gs.width)
 		for j := 0; j < gs.width; j++ {
 			neighs := gs.countNeighs(i, j)
 			if neighs < 2 || neighs > 3 {
-				newGrid[i][j] = 0
+				tmpGrid[i][j] = 0
 			}
 			if neighs == 3 {
-				newGrid[i][j] = 1
+				tmpGrid[i][j] = 1
 			}
 			if neighs == 2 {
-				newGrid[i][j] = gs.grid[i][j]
+				tmpGrid[i][j] = gs.grid[i][j]
 			}
 		}
 	}
 	for i := 0; i < gs.height; i++ {
 		for j := 0; j < gs.width; j++ {
-			gs.grid[i][j] = newGrid[i][j]
+			gs.grid[i][j] = tmpGrid[i][j]
+		}
+	}
+	return tmpGrid
+}
+
+func (gs *golState) subscribe(id int) <-chan [][]int {
+	ch := make(chan [][]int)
+	gs.sub <- subType{id, ch}
+	return ch
+}
+
+func (gs *golState) unSubscribe(id int) {
+	gs.unsub <- id
+}
+
+func (gs *golState) updateLoop() {
+	ticker := time.Tick(time.Second)
+
+	var lastGrid [][]int
+	for {
+		select {
+		case <-ticker:
+			lastGrid = gs.nextTimeStep()
+			for _, ch := range gs.updates {
+				ch <- lastGrid
+			}
+		case id := <-gs.unsub:
+			close(gs.updates[id])
+			delete(gs.updates, id)
+		case s := <-gs.sub:
+			gs.updates[s.id] = s.c
+			if lastGrid != nil {
+				s.c <- lastGrid
+			}
+		case p := <-gs.activateCell:
+			x := p.X
+			y := p.Y
+			if x >= 0 && x < gs.width && y >= 0 && y < gs.height {
+				gs.grid[y][x] = 1
+			}
 		}
 	}
 }
-
-func (gs *golState) turnOn(x, y int) {
-	if x >= 0 && x < gs.width && y >= 0 && y < gs.height {
-		gs.grid[y][x] = 1
-	}
-}
-
 func (gs *golState) countNeighs(i, j int) int {
 	neighs := 0
 	for y := -1; y <= 1; y++ {
--- ws_handler.go
+++ ws_handler.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/websocket"
+	"go.uber.org/atomic"
 )
 
 var upgrader = websocket.Upgrader{
@@ -16,25 +18,53 @@ type point struct {
 	Y int `json:"y"`
 }
 
-func wsHandler(w http.ResponseWriter, r *http.Request) {
-	c, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Print("upgrade:", err)
-		return
-	}
-	defer c.Close()
-	for {
-		var p point
-		err := c.ReadJSON(&p)
+var counter atomic.Int32
+
+func wsHandler(state golState) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		c, err := upgrader.Upgrade(w, r, nil)
 		if err != nil {
-			log.Println("read:", err)
-			break
+			log.Print("upgrade:", err)
+			return
 		}
-		log.Printf("recv: %v", p)
-		err = c.WriteJSON(p)
-		if err != nil {
-			log.Println("write:", err)
-			break
-		}
+
+		id := counter.Inc()
+		out := state.subscribe(int(id))
+
+		c.SetCloseHandler(func(code int, text string) error {
+			state.unSubscribe(int(id))
+			return nil
+		})
+
+		defer func() { _ = c.Close() }()
+		go func() {
+			for {
+				var p point
+				err := c.ReadJSON(&p)
+				if err != nil {
+					log.Println("read:", err)
+					break
+				}
+
+				state.activateCell <- p
+				log.Printf("recv: %v\n", p)
+			}
+		}()
+
+		go func() {
+			for newCells := range out {
+				s, err := json.Marshal(newCells)
+				if err != nil {
+					log.Println("Error during json marschal ", err)
+					continue
+				}
+				err = c.WriteJSON(s)
+				if err != nil {
+					log.Println("Write err: ", err)
+					continue
+				}
+			}
+		}()
+
 	}
 }
