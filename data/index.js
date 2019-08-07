4eafd5ec78069b62f1c9dbbdb67826ee67c91b91
Paris Douady <paris.douady@adevinta.com>
2019-08-07 16:21:00 +0200

    Use canvas instead of table

--- data/index.js
+++ data/index.js
@@ -1,6 +1,6 @@
 let ws;
 window.addEventListener("load", function(evt) {
-    ws = new WebSocket("ws://"+document.location.host+"/echo");
+    ws = new WebSocket("ws://" + document.location.host + "/echo");
     ws.onopen = function(evt) {
         console.log("OPEN");
     };
@@ -31,17 +31,13 @@ function receive(obj) {
     for (let i = 0; i < rows; i++) {
         for (let j = 0; j < cols; j++) {
             let val = obj[i][j];
-            if(grid[i][j] !== 2) {
+            if (grid[i][j] !== 2) {
                 grid[i][j] = val;
-                let cell = document.getElementById(i + "_" + j);
-                if(val === 0) {
-                    cell.setAttribute("class", "dead");
-                } else if(val === 1) {
-                    cell.setAttribute("class", "live");
-                }
             }
         }
     }
+
+    draw();
 }
 
 function resetGrids() {
@@ -49,8 +45,6 @@ function resetGrids() {
         for (let j = 0; j < cols; j++) {
             if (grid[i][j] === 2) {
                 grid[i][j] = 0;
-                let cell = document.getElementById(i + "_" + j);
-                cell.setAttribute("class", "dead");
             }
         }
     }
@@ -58,50 +52,8 @@ function resetGrids() {
 
 // Initialize
 function initialize() {
-    createTable();
     initializeGrids();
     resetGrids();
-}
-
-// Lay out the board
-function createTable() {
-    let gridContainer = document.getElementById('gridContainer');
-    if (!gridContainer) {
-        // Throw error
-        console.error("Problem: No div for the drid table!");
-    }
-    let table = document.createElement("table");
-
-    for (let i = 0; i < rows; i++) {
-        let tr = document.createElement("tr");
-        for (let j = 0; j < cols; j++) {//
-            let cell = document.createElement("td");
-            cell.setAttribute("id", i + "_" + j);
-            cell.setAttribute("class", "dead");
-            cell.onmouseover = cellClickHandler;
-            cell.onclick = cellClickHandler;
-            tr.appendChild(cell);
-        }
-        table.appendChild(tr);
-    }
-    gridContainer.appendChild(table);
-}
-
-function cellClickHandler(e) {
-    if(!(e.buttons === 1 || e.buttons === 3 || e.type === "click")) {
-        return
-    }
-    let rowcol = this.id.split("_");
-    let row = rowcol[0];
-    let col = rowcol[1];
-
-    if(grid[row][col] === 2) {
-        grid[row][col] = 1;
-        this.setAttribute("class", "dead");
-        return
-    }
-    this.setAttribute("class", "activate");
-    grid[row][col] = 2;
 }
 
 // clear the grid
@@ -109,21 +61,101 @@ function sendHandler() {
     for (let i = 0; i < rows; i++) {
         for (let j = 0; j < cols; j++) {
             if (grid[i][j] === 2) {
-                ws.send(JSON.stringify({x: j, y: i}))
-                let cell = document.getElementById(i + "_" + j);
-                cell.setAttribute("class", "live");
+                ws.send(JSON.stringify({
+                    x: j,
+                    y: i
+                }))
                 grid[i][j] = 1;
             }
         }
     }
 }
 
-// clear the grid
-function clearButtonHandler() {
-    console.log("Clear the game: stop playing, clear the grid");
+let canvas = document.getElementById('gridContainer');
+canvas.onmousedown = onCanvasOver;
+canvas.onmousemove = onCanvasOver;
 
+let context = document.getElementById('gridContainer').getContext('2d');
+let lastpos = {
+    x: -1,
+    y: -1
+};
+
+
+function onCanvasOver(e) {
+    let pos = getMousePos(canvas, e);
+    let x = Math.floor((pos.x - 1) / 8);
+    let y = Math.floor((pos.y - 1) / 8);
+    redrawCell(lastpos.x, lastpos.y, "")
+    lastpos = {
+        x: x,
+        y: y
+    };
+    redrawCell(x, y, "black")
+    if (!(e.buttons === 1 || e.buttons === 3 || e.type === "mousedown")) {
+        return
+    }
+    let val = grid[y][x];
+    if (val < 2 || e.type !== "mousedown") {
+        grid[y][x] = 2;
+    } else {
+        grid[y][x] = 0;
+    }
+    redrawCell(x, y, "");
+}
+
+function redrawCell(x, y, color) {
+    if (x < 0 || y < 0 || x >= cols || y >= rows) {
+        return
+    }
+    context.beginPath();
+    context.rect(x * 8, y * 8, 8, 8);
+
+    if (color === "") {
+        let cell = grid[y][x];
+        if (cell === 2) {
+            color = 'violet';
+        } else if (cell === 1) {
+            color = 'cadetblue';
+        } else {
+            color = 'white';
+        }
+    }
+    context.fillStyle = color;
+
+    context.fill();
+}
+
+function getMousePos(canvas, evt) {
+    let rect = canvas.getBoundingClientRect();
+    return {
+        x: (evt.clientX - rect.left),
+        y: (evt.clientY - rect.top)
+    };
+}
+
+function draw() {
+    context.clearRect(0, 0, 1512, 512);
+    grid.forEach(function(row, y) {
+        row.forEach(function(cell, x) {
+            context.beginPath();
+            context.rect(x * 8, y * 8, 8, 8);
+            if (x === lastpos.x && y === lastpos.y) {
+                context.fillStyle = 'black';
+            } else if (cell === 1) {
+                context.fillStyle = 'cadetblue';
+            } else if (cell === 2) {
+                context.fillStyle = 'violet';
+            } else {
+                context.fillStyle = 'white';
+            }
+            context.fill();
+        });
+    });
+}
+
+function clearButtonHandler() {
     resetGrids()
 }
 
-// Start everything
 window.onload = initialize;
