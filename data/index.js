3e226c45e8d739546decb0e37c936ed95d217bc0
Paris Douady <paris.douady@adevinta.com>
2019-08-07 10:00:54 +0200

    Separate HTML/JS/CSS

---
+++ data/index.js
@@ -0,0 +1,102 @@
+let ws;
+window.addEventListener("load", function(evt) {
+    ws = new WebSocket("ws://localhost:8080/echo");
+    ws.onopen = function(evt) {
+        console.log("OPEN");
+    };
+    ws.onclose = function(evt) {
+        console.log("CLOSE");
+        ws = null;
+    };
+    ws.onmessage = function(evt) {
+        console.log("RESPONSE: " + evt.data);
+    };
+    ws.onerror = function(evt) {
+        console.log("ERROR: " + evt.data);
+    };
+});
+let rows = 30;
+let cols = 50;
+
+let grid = new Array(rows);
+
+function initializeGrids() {
+    for (let i = 0; i < rows; i++) {
+        grid[i] = new Array(cols);
+    }
+}
+
+function resetGrids() {
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            if (grid[i][j] === 2) {
+                grid[i][j] = 0;
+                let cell = document.getElementById(i + "_" + j);
+                cell.setAttribute("class", "dead");
+            }
+        }
+    }
+}
+
+// Initialize
+function initialize() {
+    createTable();
+    initializeGrids();
+    resetGrids();
+}
+
+// Lay out the board
+function createTable() {
+    let gridContainer = document.getElementById('gridContainer');
+    if (!gridContainer) {
+        // Throw error
+        console.error("Problem: No div for the drid table!");
+    }
+    let table = document.createElement("table");
+
+    for (let i = 0; i < rows; i++) {
+        let tr = document.createElement("tr");
+        for (let j = 0; j < cols; j++) {//
+            let cell = document.createElement("td");
+            cell.setAttribute("id", i + "_" + j);
+            cell.setAttribute("class", "dead");
+            cell.onclick = cellClickHandler;
+            tr.appendChild(cell);
+        }
+        table.appendChild(tr);
+    }
+    gridContainer.appendChild(table);
+}
+
+function cellClickHandler() {
+    let rowcol = this.id.split("_");
+    let row = rowcol[0];
+    let col = rowcol[1];
+
+    this.setAttribute("class", "activate");
+    grid[row][col] = 2;
+}
+
+// clear the grid
+function sendHandler() {
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            if (grid[i][j] === 2) {
+                let cell = document.getElementById(i + "_" + j);
+                cell.setAttribute("class", "live");
+                grid[i][j] = 1;
+                ws.send(JSON.stringify({x: j, y: i}))
+            }
+        }
+    }
+}
+
+// clear the grid
+function clearButtonHandler() {
+    console.log("Clear the game: stop playing, clear the grid");
+
+    resetGrids()
+}
+
+// Start everything
+window.onload = initialize;
