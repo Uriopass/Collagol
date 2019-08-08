ad2d4e939f88a4365112e738d7497430040db365
Paris Douady <paris.douady@adevinta.com>
2019-08-08 16:23:02 +0200

    nocache

--- main.go
+++ main.go
@@ -3,11 +3,18 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"html/template"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
+	"time"
 )
 
+
+
 func main() {
+	rand.Seed(time.Now().Unix())
 	gol := newGolState()
 	go gol.updateLoop()
 	log.SetFlags(0)
@@ -21,7 +28,26 @@ func main() {
 		s, _ := json.Marshal(connected)
 		_, _ = writer.Write(s)
 	})
-	http.Handle("/", http.FileServer(http.Dir("data/")))
+	http.HandleFunc("/", serveIndex)
+	http.HandleFunc("/index.html", serveIndex)
+	http.Handle("/data/", http.FileServer(http.Dir(".")))
 	fmt.Println("Init ok")
 	log.Fatal(http.ListenAndServe(":80", nil))
+}
+
+type rStruct struct {
+	R1 string
+	R2 string
+}
+
+func serveIndex(writer http.ResponseWriter, request *http.Request) {
+	t := template.New("index.html")
+	t, _ = t.ParseFiles("data/index.html")
+
+	r := rStruct{
+		R1: strconv.Itoa(rand.Int()),
+		R2: strconv.Itoa(rand.Int()),
+	}
+
+	_ = t.Execute(writer, r)
 }
