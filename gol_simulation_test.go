2707a2146d975646faf81e7114346848484ee7ff
Douady Pâris <paris.douady@hotmail.fr>
2019-08-09 08:35:44 +0200

    'bench'

--- gol_simulation_test.go
+++ gol_simulation_test.go
@@ -7,8 +7,8 @@ import (
 
 func BenchmarkTimestep(t *testing.B) {
 	gs := newGolState(config{
-		Width:  100,
-		Height: 100,
+		Width:  1000,
+		Height: 1000,
 	})
 
 	rand.Seed(0)
@@ -17,13 +17,6 @@ func BenchmarkTimestep(t *testing.B) {
 		tmpGrid[i] = rand.Int() % 2
 	}
 	for k := 0; k < t.N; k++ {
-		for i := 0; i < gs.height; i++ {
-			for j := 0; j < gs.width; j++ {
-				gs.grid[i][j] = tmpGrid[i*gs.width+j]
-			}
-		}
-		for i := 0; i < 10; i++ {
-			gs.nextTimeStep()
-		}
+		gs.nextTimeStep()
 	}
 }
