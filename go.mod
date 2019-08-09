34c251f3a7b972373e4e06f5ab3e42bd0874cb3d
Paris Douady <paris.douady@adevinta.com>
2019-08-09 15:49:54 +0200

    Use LZME compression

--- go.mod
+++ go.mod
@@ -5,6 +5,9 @@ go 1.12
 require (
 	github.com/gomodule/redigo v2.0.0+incompatible
 	github.com/gorilla/websocket v1.4.0
+	github.com/itchio/lzma v0.0.0-20190703113020-d3e24e3e3d49
 	github.com/microcosm-cc/bluemonday v1.0.2
+	github.com/stretchr/testify v1.3.0 // indirect
 	go.uber.org/atomic v1.4.0
+	golang.org/x/tools v0.0.0-20190808195139-e713427fea3f
 )
--- main/ws_handler.go
+++ main/ws_handler.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
+	"io"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/websocket"
+	"github.com/itchio/lzma"
 	"go.uber.org/atomic"
 )
 
@@ -19,6 +22,30 @@ var upgrader = websocket.Upgrader{
 type point [2]int
 
 var counter atomic.Int32
+
+func compress(tt []byte) []byte {
+	b := new(bytes.Buffer)
+
+	pr, pw := io.Pipe()
+	defer pr.Close()
+
+	in := bytes.NewBuffer(tt)
+	size := int64(len(tt))
+
+	go func() {
+		defer pw.Close()
+
+		w := lzma.NewWriterSizeLevel(pw, size, lzma.BestSpeed)
+		defer w.Close()
+
+		_, err := io.Copy(w, in)
+		if err != nil {
+			log.Printf("error encoding: %v\n", err)
+		}
+	}()
+	_, _ = io.Copy(b, pr)
+	return b.Bytes()
+}
 
 func wsHandler(state *golState) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -64,7 +91,12 @@ func wsHandler(state *golState) func(w http.ResponseWriter, r *http.Request) {
 		}()
 
 		for newCells := range out {
-			err = c.WriteJSON(newCells)
+			payload, err := json.Marshal(newCells)
+			if err != nil {
+				log.Println("json err: ", err)
+			}
+			compressed := compress(payload)
+			err = c.WriteMessage(websocket.BinaryMessage, compressed)
 			if err != nil {
 				log.Println("Write err: ", err)
 				break
